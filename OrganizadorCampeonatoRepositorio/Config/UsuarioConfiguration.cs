3fcb90be7cf61a6088a225a4f6959cb86836f945
Kesley de Mello <kesley_ksl@hotmail.com>
2019-11-17 14:19:57 -0300

    Iniciada configurações do Entity

---
+++ OrganizadorCampeonatoRepositorio/Config/UsuarioConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrganizadorCampeonatoDominio.Entidades;
+
+namespace OrganizadorCampeonatoRepositorio.Config
+{
+    public class UsuarioConfiguration : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ OrganizadorCampeonatoRepositorio/Contexto/OrganizadorCampeonatoContexto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrganizadorCampeonatoDominio.Entidades;
+using OrganizadorCampeonatoDominio.ObjetoDeValor;
+
+namespace OrganizadorCampeonatoRepositorio.Contexto
+{
+    public class OrganizadorCampeonatoContexto : DbContext
+
+    {
+
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Campeonato> Campeonatos { get; set; }
+        public DbSet<Fase> Fases { get; set; }
+        public DbSet<Musica> Musicas { get; set; }
+        public DbSet<UsuarioFase> UsuariosFase { get; set; }
+        public DbSet<TipoFase> TiposFase { get; set; }
+
+        public OrganizadorCampeonatoContexto(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
